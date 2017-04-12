a8046a2ac27f959cb4e976317e7c30d4b2b3bd51
Mihai Grajdeanu <mihai.grajdeanu@softescu.com>
2017-04-12 13:52:13 +0300

    first

---
+++ sites/all/themes/nadonitheme/templates/page.tpl.php
@@ -0,0 +1,4 @@
+<video  width="100%" height="100%"  centered   loop controls style="position:fixed; top:0px; background-color:black">
+  <source src="sites\all\themes\nadonitheme\templates\movie.mp4" type="video/mp4">
+
+</video>
