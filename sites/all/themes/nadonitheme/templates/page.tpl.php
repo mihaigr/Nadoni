1282d137a761b63473aca3243054abb87b22ed40
Mihai Grajdeanu <mihai.grajdeanu@softescu.com>
2017-04-12 14:18:31 +0300

    Deleted page.tpl.php

--- sites/all/themes/nadonitheme/templates/page.tpl.php
+++
@@ -1,4 +0,0 @@
-<video  width="100%" height="100%"  centered   loop controls style="position:fixed; top:0px; background-color:black">
-  <source src="sites\all\themes\nadonitheme\templates\movie.mp4" type="video/mp4">
-
-</video>
